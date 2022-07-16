9298276508215c512e91177c1158df56d09d6fff
agchan_san <yuuki617phone@gmail.com>
2022-07-16 16:09:22 +0900

    パイプの必要数を検出

--- calling.c
+++ calling.c
@@ -7,15 +7,37 @@
 #include <sys/wait.h>
 
 int call(char **argv){
+        //pipeでつなぐやつをやる
+        //必要数を検出
+        int pipes = 0;
+        int *pipe = (int*)malloc(DEFAULT_MAXPIPES * sizeof(int));
+        int pc = DEFAULT_MAXPIPES;
+        for(int i=0;i<argv[i] != NULL;i++){
+                if(strcmp(argv[i],"|") == 0){
+                        argv[i] = NULL;
+                        if(pipes > pc){
+                                pc += DEFAULT_MAXPIPES;
+                                pipe = realloc(pipe,pc);
+                        }
+                        pipe[pipes++] = i;
+                }
+        }
+        for(int i=0;i<pc;i++){
+                fprintf(stderr,"%d ",pipe[i]);
+        }
+
+
         pid_t pid;
         pid = fork();
         if(pid<0){
                 fprintf(stderr,"fork(2) failed\n");
+                free(pipe);
                 return 1;
         }
         if(pid == 0){
                 execvp(argv[0],argv);
                 perror(argv[0]);
+                free(pipe);
                 return 99;
         }else{
                 int status;
@@ -28,6 +50,7 @@ int call(char **argv){
                 }else{
                         fprintf(stderr,"abnormal exit\n");
                 }
-                return 0;
         }
+        
+        free(pipe);
 }
--- calling.h
+++ calling.h
@@ -1,6 +1,8 @@
 #ifndef CALLING_H
 #define CALLING_H
 
+#define DEFAULT_MAXPIPES 10
+
 int call(char **argv);
 
 
