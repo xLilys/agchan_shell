8ccda5e704f94eb7b1e03e72f8532ed7a0cf3911
agchan_san <yuuki617phone@gmail.com>
2022-07-16 15:15:53 +0900

    strtestの出力をprintからfprintfに変更

--- ui.h
+++ ui.h
@@ -4,7 +4,7 @@
 #define INPUT_BUFSIZE 256
 #define PATH_BUFSIZE 256
 
-#define strtest(str) for(int i=0;str[i] != '\0';i++)printf("%c%c",str[i],str[i+1] != '\0' ? ' ':'\n');
+#define strtest(str) for(int i=0;str[i] != '\0';i++)fprintf(stderr,"%c%c",str[i],str[i+1] != '\0' ? ' ':'\n');
 	
 
 //入力された文字列を読み取る。メモリが動的確保されるので要free()
