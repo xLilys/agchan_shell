2074658b4d02146c0deddfb2f14ac824adce8e34
agchan <yuuki617phone@gmail.com>
2022-07-18 20:16:09 +0900

    mainファイルでのsignal.hのincludeをやめる

--- signal_handle.c
+++ signal_handle.c
@@ -1,3 +1,9 @@
 #include "signal_handle.h"
 
 
+signalhandler_t trap_signal(int sig,signalhandler_t handler){
+    struct sigaction act,old;
+}
+
+
+
--- signal_handle.h
+++ signal_handle.h
@@ -1,5 +1,10 @@
 #ifndef SIGHANDLE_H
 #define SIGHANDLE_H
 
+#include <signal.h>
+
+typedef void (*signalhandler_t)(int);
+
+
 
 #endif
