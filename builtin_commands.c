5784ee365347ef35238d50be064aad966426628e
agchan <yuuki617phone@gmail.com>
2022-07-19 10:54:54 +0900

    exitコマンドを実装

--- builtin_commands.c
+++ builtin_commands.c
@@ -10,16 +10,11 @@ int __bcs_func__(int argc,char **argv);
 
 
 //登録した関数の数-------
-#define BUILTIN_FUNCS 1
+#define BUILTIN_FUNCS 2
 //----------------------
 
-//プロトタイプ宣言########################################################################################################################################################################################
-int cd(int argc,char **argv);
-//#######################################################################################################################################################################################################
-
-
 //ここに関数ポインタを追加
-int (*bcs[BUILTIN_FUNCS])(int argc,char **argv) = {cd};
+int (*bcs[BUILTIN_FUNCS])(int argc,char **argv) = {cd,exit_shell};
 
 
 /*実装のルール
@@ -38,6 +33,27 @@ int cd(int argc,char **argv){
                 }
             }else{
                 fprintf(stderr,"too few arguments.");
+            }
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int exit_shell(int argc,char **argv){
+    if(argc > 0){
+        if(strcmp(argv[0],"exit") == 0){
+            if(argc > 1){
+                int exit_status = (int)atol(argv[1]);
+                if(exit_status == 0L){
+                    fprintf(stderr,"invalid exit status=%s\n",argv[1]);
+                }else{
+                    exit(exit_status);
+                }
+            }else if(argc > 2){
+                fprintf(stderr,"too many arguments.\n");
+            }else{
+                exit(0);
             }
             return 1;
         }
